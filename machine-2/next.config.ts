b256e15037009838a3900f57e228613d9274015d
ashlee <ashleina20@gmail.com>
2026-08-04 17:49:10 +0800

    Update next.config.ts

--- machine-2/next.config.ts
+++ machine-2/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   images: { unoptimized: true },
   ...(process.env.GITHUB_ACTIONS
     ? { basePath: "/CSARCH2ComputingMachine2", assetPrefix: "/CSARCH2ComputingMachine2/" }
