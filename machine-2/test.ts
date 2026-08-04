06f091c7f5b595a4ddac83e404e341a4751a4740
draezzz <kdvsgiain@gmail.com>
2026-08-04 18:37:58 +0800

    Fix nested git repository

--- machine-2/test.ts
+++ machine-2/test.ts
@@ -33,6 +33,9 @@ console.log(`Input -8.75: ${JSON.stringify(convert(-8.75))}`);
 console.log(`Round Binary Unsigned Whole Tail>Mid (11101, target 2): ${JSON.stringify(roundingMethods("11101", "unsigned", "-1", "2", "binary"))}`);
 console.log(`Round Binary Unsigned Whole Exact Tie (11101, target 2): ${JSON.stringify(roundingMethods("11101", "signed", "1", "2", "binary"))}`);
 
+// Binary Error
+console.log(`Round Binary Unsigned Whole Exact Tie (11101, target 2): ${JSON.stringify(roundingMethods("aaa", "signed", "1", "2", "binary"))}`);
+
 // Binary - Fractions & Tie Breaks (LSB Parity)
 console.log(`Round Binary Tie to Odd LSB (1.011, target 2): ${JSON.stringify(roundingMethods("1.011", "unsigned", "-1", "2", "binary"))}`);
 console.log(`Round Binary Tie to Even LSB (1.101, target 2): ${JSON.stringify(roundingMethods("1.101", "unsigned", "-1", "2", "binary"))}`);
